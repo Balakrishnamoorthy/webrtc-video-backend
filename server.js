7815dfdd77df285a42643c2907d46497efbfea15
Balakrishnamoorthy <balakrishnamoorthy77@gmail.com>
2026-02-20 16:31:21 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("ready");
+  });
+
+  socket.on("offer", ({ roomId, offer }) => {
+    socket.to(roomId).emit("offer", offer);
+  });
+
+  socket.on("answer", ({ roomId, answer }) => {
+    socket.to(roomId).emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", ({ roomId, candidate }) => {
+    socket.to(roomId).emit("ice-candidate", candidate);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("leave-room", (roomId) => {
+    socket.to(roomId).emit("call-ended");
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
